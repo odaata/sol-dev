1108c5852be5a0a37c8968acd8e38c400da27158
Mike Olson <mike@madtownmining.com>
2024-07-15 23:32:19 -0500

    Add more tests for each action. Fix issues with decrement.

--- src/instructions.rs
+++ src/instructions.rs
@@ -2,13 +2,23 @@ use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::program_error::ProgramError;
 
 #[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct IncrementArgs {
+    pub value: u32,
+}
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
+pub struct DecrementArgs {
+    pub value: u32,
+}
+
+#[derive(Debug, BorshDeserialize, BorshSerialize)]
 pub struct UpdateArgs {
     pub value: u32,
 }
 
 pub enum CounterInstructions {
-    Increment,
-    Decrement,
+    Increment(IncrementArgs),
+    Decrement(DecrementArgs),
     Update(UpdateArgs),
     Reset,
 }
@@ -20,8 +30,8 @@ impl CounterInstructions {
             .ok_or(ProgramError::InvalidInstructionData)?;
 
         Ok(match variant {
-            0 => Self::Increment,
-            1 => Self::Decrement,
+            0 => Self::Increment(IncrementArgs::try_from_slice(rest).unwrap()),
+            1 => Self::Decrement(DecrementArgs::try_from_slice(rest).unwrap()),
             2 => Self::Update(UpdateArgs::try_from_slice(rest).unwrap()),
             3 => Self::Reset,
             _ => return Err(ProgramError::InvalidInstructionData),
--- src/lib.rs
+++ src/lib.rs
@@ -33,12 +33,17 @@ pub fn process_instruction(
     let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
 
     match instruction {
-        CounterInstructions::Increment => {
-            counter_account.counter += 1;
+        CounterInstructions::Increment(args) => {
+            counter_account.counter += args.value;
         }
-        CounterInstructions::Decrement => {
-            counter_account.counter -= 1;
-        }
+        CounterInstructions::Decrement(args) => match args.value > counter_account.counter {
+            true => {
+                counter_account.counter = 0;
+            }
+            false => {
+                counter_account.counter -= args.value;
+            }
+        },
         CounterInstructions::Reset => {
             counter_account.counter = 0;
         }
@@ -58,7 +63,7 @@ mod test {
     use std::mem;
 
     #[test]
-    fn test_counter() {
+    fn test_increment() {
         let program_id = Pubkey::default();
         let key = Pubkey::default();
         let mut lamports = 0;
@@ -79,63 +84,141 @@ mod test {
         let accounts = vec![account];
 
         let mut increment_instruction_data: Vec<u8> = vec![0];
-        let mut decrement_instruction_data: Vec<u8> = vec![1];
-        let mut update_instruction_data: Vec<u8> = vec![2];
-        let reset_instruction_data: Vec<u8> = vec![3];
-
         let increment_value = 48u32;
         increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
         process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
 
-        assert_eq!(
-            CounterAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            48
+        let increment_result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(increment_result, 48);
+    }
+
+    #[test]
+    fn test_decrement() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
         );
 
-        let decrement_value = 16u32;
-        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
+        let accounts = vec![account];
+
+        let mut increment_instruction_data: Vec<u8> = vec![0];
+        let increment_value = 48u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+
+        let increment_result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(increment_result, 48);
+
+        let mut decrement_instruction_data: Vec<u8> = vec![1];
+        let value = 16u32;
+        decrement_instruction_data.extend_from_slice(&value.to_le_bytes());
         process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
-
-        assert_eq!(
-            CounterAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            32
-        );
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 32);
 
         let mut big_decrement_instruction_data: Vec<u8> = vec![1];
-        let big_decrement_value = 100u32;
-        big_decrement_instruction_data.extend_from_slice(&big_decrement_value.to_le_bytes());
+        big_decrement_instruction_data.extend_from_slice(&100u32.to_le_bytes());
         process_instruction(&program_id, &accounts, &big_decrement_instruction_data).unwrap();
 
-        assert_eq!(
-            CounterAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            0
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_update_counter() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
         );
 
-        let update_value = 33u32;
-        update_instruction_data.extend_from_slice(&update_value.to_le_bytes());
+        let accounts = vec![account];
 
-        process_instruction(&program_id, &accounts, &update_instruction_data).unwrap();
+        let mut increment_instruction_data: Vec<u8> = vec![0];
+        let increment_value = 48u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 48);
 
-        assert_eq!(
-            CounterAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            33
+        let mut instruction_data: Vec<u8> = vec![2];
+        instruction_data.extend_from_slice(&33u32.to_le_bytes());
+        process_instruction(&program_id, &accounts, &instruction_data).unwrap();
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 33);
+    }
+
+    #[test]
+    fn test_reset_counter() {
+        let program_id = Pubkey::default();
+        let key = Pubkey::default();
+        let mut lamports = 0;
+        let mut data = vec![0; mem::size_of::<u32>()];
+        let owner = Pubkey::default();
+
+        let account = AccountInfo::new(
+            &key,
+            false,
+            true,
+            &mut lamports,
+            &mut data,
+            &owner,
+            false,
+            Epoch::default(),
         );
 
+        let accounts = vec![account];
+
+        let mut increment_instruction_data: Vec<u8> = vec![0];
+        let increment_value = 48u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
+        process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 48);
+
+        let reset_instruction_data: Vec<u8> = vec![3];
         process_instruction(&program_id, &accounts, &reset_instruction_data).unwrap();
 
-        assert_eq!(
-            CounterAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            0
-        );
+        let result = CounterAccount::try_from_slice(&accounts[0].data.borrow())
+            .unwrap()
+            .counter;
+        assert_eq!(result, 0);
     }
 }
